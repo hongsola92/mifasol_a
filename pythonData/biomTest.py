38cf25277ea8c4bb8ee19c974738d7ccc3bc3d10
hongsola92 <thfdkwl@kakao.com>
2024-05-29 15:57:30 +0900

    지수분포 포함

--- pythonData/biomTest.py
+++ pythonData/biomTest.py
@@ -8,5 +8,11 @@ def main():
     # CDF 계산 (2번 이하 성공, 5번 시도, 성공 확률 0.1)
     print(stats.binom.cdf(2, n=5, p=0.1))
 
+    # 푸아송 분포
+    print(stats.poisson.rvs(2, size=100))
+    print(stats.poisson.rvs(2, size=100).mean())
+    # 지수분포
+    print(stats.expon.rvs(scale=1/0.2, size = 100).mean())
+
 if __name__ == "__main__":
     main()
